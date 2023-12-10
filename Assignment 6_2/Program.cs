eeaf60ba64de1c6441b1116f099876f8ff7ab3c1
Sunil Dagur <sunildagur566@gmail.com>
2023-12-10 19:29:08 +0530

    Add project files.

---
+++ Assignment 6_2/Program.cs
@@ -0,0 +1,63 @@
+﻿
+using System;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace StudentManagementSystem
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string username = txtUsername.Text;
+            string password = HashPassword(txtPassword.Text); 
+            if (AuthenticateUser(username, password))
+            {
+                MessageBox.Show("Login successful!");
+                
+            }
+            else
+            {
+                MessageBox.Show("Invalid username or password. Please try again.");
+            }
+        }
+
+        private bool AuthenticateUser(string username, string password)
+        {
+            using (SqlConnection connection = new SqlConnection("YourConnectionStringHere"))
+            {
+                connection.Open();
+
+                string query = "SELECT * FROM Faculty WHERE Username = @Username AND Password = @Password";
+
+                using (SqlCommand cmd = new SqlCommand(query, connection))
+                {
+                    cmd.Parameters.AddWithValue("@Username", username);
+                    cmd.Parameters.AddWithValue("@Password", password);
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        return reader.HasRows;
+                    }
+                }
+            }
+        }
+
+        private string HashPassword(string password)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] hashedBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+                return BitConverter.ToString(hashedBytes).Replace("-", "").ToLower();
+            }
+        }
+    }
+}
+
